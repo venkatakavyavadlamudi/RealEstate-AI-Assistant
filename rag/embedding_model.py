f34c8e81285c8cfcbe9f0fed1432b7112cd8b020
Kasim <kasimdarling25@gmail.com>
2026-07-14 14:33:01 +0530

    Debug embedding key

--- rag/embedding_model.py
+++ rag/embedding_model.py
@@ -3,6 +3,10 @@ from utils.config import GOOGLE_API_KEY, EMBEDDING_MODEL
 
 
 def get_embedding_model():
+
+    print("API KEY EXISTS:", GOOGLE_API_KEY is not None)
+    print("MODEL:", EMBEDDING_MODEL)
+
     return GoogleGenerativeAIEmbeddings(
         model=EMBEDDING_MODEL,
         google_api_key=GOOGLE_API_KEY
